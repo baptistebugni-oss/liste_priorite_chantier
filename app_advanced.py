e9726c310408395ca24710ddadd706d2e9b4a58f
baptistebugni-oss <baptistebugni@gmail.com>
2025-12-07 18:53:40 +0100

    Update app_advanced.py

--- app_advanced.py
+++ app_advanced.py
@@ -297,7 +297,7 @@ def build_pdf_liste(df, opts, qr_url=None):
         draw_circle(
             c,
             margin_left + col_widths[0] / 2,
-            y - 6,
+            y - 3,
             4,
             urg_hex
         )
@@ -307,7 +307,7 @@ def build_pdf_liste(df, opts, qr_url=None):
         draw_circle(
             c,
             margin_left + sum(col_widths) - col_widths[-1] / 2,
-            y - 6,
+            y - 3,
             4,
             stat_hex
         )
